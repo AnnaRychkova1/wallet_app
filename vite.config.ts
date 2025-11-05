64a8b68b5d7afa4bef0aeb06022dcbbda1dc8b78
Anna Rychkova <aanytkaa@gmail.com>
2025-11-05 04:03:45 +0000

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ command }) => {
+  return {
+    base: command === "serve" ? "/" : "/wallet_app/", // dev = "/", build = "/wallet_app/"
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
+  };
 });
